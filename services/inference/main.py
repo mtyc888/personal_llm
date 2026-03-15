f87c2564f5e18de4e4efd68e8b2a84dcede38f2d
marvin <ymarvintan@gmail.com>
2026-03-15 23:53:33 +0800

    initial commit

--- services/inference/main.py
+++ services/inference/main.py
@@ -8,30 +8,6 @@ class GenerateReq(BaseModel):
     user_query: str
     data_list: list
 
-@app.post("/generate-data")
-def generateData(request: GenerateReq):
-    context = sorted(request.data_list, key=lambda x : x['distances'])
-    context_text = "\n---\n".join(item['content'] for item in context[:3])
-    question = request.user_query
-
-    response = ollama.chat(
-        model='llama3.1', 
-        messages=[
-        {
-            'role': 'system',
-            'content': 'You are a personal assistant. Use the provided context to answer the user. '
-                    'If the answer is not in the context, say you do not know. '
-                    'Always cite your source if available in the context.'
-        },
-        {
-            'role': 'user',
-            'content': f"Context:\n{context_text}\n\nQuestion: {request.user_query}"
-        },
-        ]    
-    )
-
-    return response['message']['content']
-
 @app.post("/generate")
 def generate(request: GenerateReq):
     context = sorted(request.data_list, key=lambda x : x['distances'])
